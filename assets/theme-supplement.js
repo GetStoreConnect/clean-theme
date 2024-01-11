e70de1840900d14fc39df8d8059a7ec6b8b37522
Aaron Matawaran <aaronryematawaran@gmail.com>
2024-01-11 10:17:50 +0800

    New block FAQ item, icon, updated theme supplement css and js

--- assets/theme-supplement.js
+++ assets/theme-supplement.js
@@ -1,30 +1,71 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   let e = (function () {
-      let t = { items: 1, responsive: { 768: { items: 3 }, 992: { items: 4 }, 1700: { items: 5 } }, gutter: 20, nav: !1, swipeAngle: !1, speed: 400 },
-        r = [];
+      let t = {
+          items: 1,
+          responsive: {
+            768: { items: 3 },
+            992: { items: 4 },
+            1700: { items: 5 },
+          },
+          gutter: 20,
+          nav: !1,
+          swipeAngle: !1,
+          speed: 400,
+        },
+        r = []
       return (
         $(() => {
-          document.querySelectorAll("[data-featured-slider]") &&
-            [...document.querySelectorAll("[data-featured-slider]")].map((t) => {
-              e.init("[data-featured-slider=" + t.getAttribute("data-featured-slider") + "]");
-            });
+          document.querySelectorAll('[data-featured-slider]') &&
+            [...document.querySelectorAll('[data-featured-slider]')].map(
+              (t) => {
+                e.init(
+                  '[data-featured-slider=' +
+                    t.getAttribute('data-featured-slider') +
+                    ']'
+                )
+              }
+            )
         }),
         {
           init(e, i) {
-            let s = tns({ ...t, ...i, container: e });
-            r.push(s);
+            let s = tns({ ...t, ...i, container: e })
+            r.push(s)
           },
         }
-      );
+      )
     })(),
-    t = document.querySelector("#sc-header"),
+    t = document.querySelector('#sc-header'),
     r = t.getBoundingClientRect().height / 2,
-    i = document.querySelector("nav.sc-header_inner"),
+    i = document.querySelector('nav.sc-header_inner'),
     s = (e) => {
       e.forEach((e) => {
-        e.target, e.isIntersecting ? i.classList.remove("sticky") : i.classList.add("sticky");
-      });
+        e.target,
+          e.isIntersecting
+            ? i.classList.remove('sticky')
+            : i.classList.add('sticky')
+      })
     },
-    a = new IntersectionObserver(s, { root: null, threshold: 0.5, rootMargin: `${r}px` });
-  a.observe(t);
-});
+    a = new IntersectionObserver(s, {
+      root: null,
+      threshold: 0.5,
+      rootMargin: `${r}px`,
+    })
+  a.observe(t)
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+  const faqAccordions = document.querySelectorAll('.accordion')
+
+  faqAccordions.forEach((accordion) => {
+    accordion.addEventListener('click', function () {
+      this.classList.toggle('active-accordion')
+      const panel = this.nextElementSibling
+
+      if (panel.style.display === 'block') {
+        panel.style.display = 'none'
+      } else {
+        panel.style.display = 'block'
+      }
+    })
+  })
+})
