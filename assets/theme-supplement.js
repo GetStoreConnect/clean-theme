2c26a174d45f12e622ee556b864d3a1e4ba611cc
Aaron Matawaran <aaronryematawaran@gmail.com>
2025-03-17 15:44:38 +0800

    Updated CSS, reverted class and script

--- assets/theme-supplement.js
+++ assets/theme-supplement.js
@@ -1,51 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let slider = (function () {
-    let defaults = {
-        items: 1,
-        responsive: {
-          768: { items: 3 },
-          992: { items: 4 },
-          1700: { items: 5 }
-        },
-        gutter: 20,
-        nav: false,
-        swipeAngle: false,
-        speed: 400
-      },
-      sliders = [];
-
-    return (
-      $(() => {
-        document.querySelectorAll("[data-featured-slider]") &&
-          [...document.querySelectorAll("[data-featured-slider]")].map((el) => {
-            slider.init("[data-featured-slider=" + el.getAttribute("data-featured-slider") + "]");
-          });
-      }),
-      {
-        init(container, options) {
-          let tinySlider = tns({ ...defaults, ...options, container: container });
-          sliders.push(tinySlider);
+  let e = (function () {
+      let t = { items: 1, responsive: { 768: { items: 3 }, 992: { items: 4 }, 1700: { items: 5 } }, gutter: 20, nav: !1, swipeAngle: !1, speed: 400 },
+        r = [];
+      return (
+        $(() => {
+          document.querySelectorAll("[data-featured-slider]") &&
+            [...document.querySelectorAll("[data-featured-slider]")].map((t) => {
+              e.init("[data-featured-slider=" + t.getAttribute("data-featured-slider") + "]");
+            });
+        }),
+        {
+          init(e, i) {
+            let s = tns({ ...t, ...i, container: e });
+            r.push(s);
+          },
         }
-      }
-    );
-  })();
-
-  const header = document.querySelector("#sc-header");
-  const nav = document.querySelector("nav.sc-header_inner");
-  let isSticky = false;
-
-  const handleSticky = () => {
-    const scrollPosition = window.scrollY;
-
-    if (scrollPosition > 0 && !isSticky) {
-      nav.classList.add("sticky");
-      isSticky = true;
-    } else if (scrollPosition === 0 && isSticky) {
-      nav.classList.remove("sticky");
-      isSticky = false;
-    }
-  };
-
-  window.addEventListener("scroll", handleSticky);
-  handleSticky();
+      );
+    })(),
+    t = document.querySelector("#sc-header"),
+    r = t.getBoundingClientRect().height / 2,
+    i = document.querySelector("nav.sc-header_inner"),
+    s = (e) => {
+      e.forEach((e) => {
+        e.target, e.isIntersecting ? i.classList.remove("sticky") : i.classList.add("sticky");
+      });
+    },
+    a = new IntersectionObserver(s, { root: null, threshold: 0.5, rootMargin: `${r}px` });
+  a.observe(t);
 });
